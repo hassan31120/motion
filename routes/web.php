5b776faee1356d2543af134fa472c1cb3fc0913a
Mohammed Hassan <medo.hassan0100@gmail.com>
2022-10-01 19:27:35 +0200

    second

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\LinksController;
+use App\Http\Controllers\Admin\UsersController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +23,29 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['prefix' => 'admin', 'middleware' => 'isAdmin'], function () {
+    // Users
+    Route::get('/users', [UsersController::class, 'index'])->name('admin.users');
+    Route::get('/user/create', [UsersController::class, 'create'])->name('admin.user.create');
+    Route::post('/user/store', [UsersController::class, 'store'])->name('admin.user.store');
+    Route::get('/user/edit/{id}', [UsersController::class, 'edit'])->name('admin.user.edit');
+    Route::post('/user/update/{id}', [UsersController::class, 'update'])->name('admin.user.update');
+    Route::get('/user/destroy/{id}', [UsersController::class, 'destroy'])->name('admin.user.destroy');
+
+    // Admins
+    Route::get('/admins', [UsersController::class, 'Adminindex'])->name('admin.admins');
+    Route::get('/admin/create', [UsersController::class, 'Admincreate'])->name('admin.admin.create');
+    Route::post('/admin/store', [UsersController::class, 'Adminstore'])->name('admin.admin.store');
+    Route::get('/admin/edit/{id}', [UsersController::class, 'Adminedit'])->name('admin.admin.edit');
+    Route::post('/admin/update/{id}', [UsersController::class, 'Adminupdate'])->name('admin.admin.update');
+    Route::get('/admin/destroy/{id}', [UsersController::class, 'Admindestroy'])->name('admin.admin.destroy');
+
+     // Links
+     Route::get('/links', [LinksController::class, 'index'])->name('admin.links');
+     Route::get('/link/create', [LinksController::class, 'create'])->name('admin.link.create');
+     Route::post('/link/store', [LinksController::class, 'store'])->name('admin.link.store');
+     Route::get('/link/edit/{id}', [LinksController::class, 'edit'])->name('admin.link.edit');
+     Route::post('/link/update/{id}', [LinksController::class, 'update'])->name('admin.link.update');
+     Route::get('/link/destroy/{id}', [LinksController::class, 'destroy'])->name('admin.link.destroy');
+});
